42553182d0a1ce6a03df22785f53b6839d8044e7
mputzi <mputzi@gmx.de>
2013-08-12 10:23:00 +0200

    GUI-Elemente erzeugt

---
+++ src/korrgui/Hauptfenster.java
@@ -0,0 +1,48 @@
+package korrgui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+
+public class Hauptfenster {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Hauptfenster window = new Hauptfenster();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Hauptfenster() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(12, 0, 129, 21);
+		frame.getContentPane().add(menuBar);
+	}
+}

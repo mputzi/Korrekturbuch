d33fa52f9a2ee6c56dc161bcfaed64f4c5df40cf
mputzi <mputzi@gmx.de>
2013-08-12 10:35:03 +0200

    menu bar main window created

--- src/korrgui/Hauptfenster.java
+++ src/korrgui/Hauptfenster.java
@@ -4,6 +4,10 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Hauptfenster {
 
@@ -42,7 +46,35 @@ public class Hauptfenster {
 		frame.getContentPane().setLayout(null);
 		
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(12, 0, 129, 21);
+		menuBar.setBounds(0, 0, 436, 21);
 		frame.getContentPane().add(menuBar);
+		
+		JMenu mnDatei = new JMenu("Datei");
+		menuBar.add(mnDatei);
+		
+		JMenuItem mntmPasswortndern = new JMenuItem("Passwort ändern");
+		mnDatei.add(mntmPasswortndern);
+		
+		JMenuItem mntmBeenden = new JMenuItem("Beenden");
+
+		mnDatei.add(mntmBeenden);
+		
+		JMenu mnKlasse = new JMenu("Klasse");
+		menuBar.add(mnKlasse);
+		
+		JMenuItem mntmffnen = new JMenuItem("Öffnen");
+		//mntmffnen.setEnabled(false);
+		mnKlasse.add(mntmffnen);
+		
+		
+		JMenuItem mntmNeuAnlegen = new JMenuItem("Neu anlegen");
+		mnKlasse.add(mntmNeuAnlegen);
+		
+		JMenu mnPrfung = new JMenu("Prüfung");
+		menuBar.add(mnPrfung);
+		mnPrfung.setEnabled(false);
+		
+		JMenu mnNeu = new JMenu("Neu");
+		mnPrfung.add(mnNeu);
 	}
 }

2df365c6c11b90fe466f1b4385e7dd2e84941fe2
Rainer <Rainer@Rainer-Dell>
2013-10-28 13:23:31 +0100

    Klassenauswahl mit Zusammenfassung

--- src/korrgui/KlasseOeffnen.java
+++ src/korrgui/KlasseOeffnen.java
@@ -5,16 +5,29 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import javax.swing.UIManager;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.AbstractListModel;
 
 public class KlasseOeffnen extends JDialog {
 
 	Frame aufrufer = new Frame();
 	private final JPanel contentPanel = new JPanel();
+	private JTextArea Zusammenfassung = new JTextArea();
+	private JList<String> Klassenliste = new JList<String> ();
+	
+	
 
 	/**
 	 * Launch the application.
@@ -34,8 +47,10 @@ public class KlasseOeffnen extends JDialog {
 	 * Create the dialog.
 	 */
 	public KlasseOeffnen(final Frame aufrufer) {
+		setResizable(false);
 		this.aufrufer = aufrufer;
 		initialize(); // Dialogfenster aufbauen mit "OK"- und "Cancel"-Button
+		//set_Zusammenfassung("Test");
 	}
 	
 	private ActionListener CLASSal = new ActionListener(){
@@ -74,13 +89,60 @@ public class KlasseOeffnen extends JDialog {
 		aufrufer.setVisible(true);
 	}
 	
+	public void set_Zusammenfassung (String arg){
+		Zusammenfassung.setText(arg);
+	}
+	
+	public void set_Klassenliste (String[] txt){
+		//Test
+	}
+	
+	
 	private void initialize() {
 		setTitle("Klasse öffnen");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JScrollPane scrollPane = new JScrollPane();
+			scrollPane.setPreferredSize(new Dimension(150, 200));
+			scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			contentPanel.add(scrollPane, BorderLayout.WEST);
+			{
+				//JList Klassenliste = new JList();
+				Klassenliste.setModel(new AbstractListModel() {
+					String[] values = new String[] {"Tets", "Auto", "Banane", "dksjge", "dsaökfe", "dddddjjj", "Tets", "Auto", "Banane", "dksjge", "dsaökfe", "dddddjjj", "Tets", "Auto", "Banane", "dksjge", "dsaökfe", "dddddjjj"};
+					public int getSize() {
+						return values.length;
+					}
+					public Object getElementAt(int index) {
+						return values[index];
+					}
+				});
+				Klassenliste.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				scrollPane.setViewportView(Klassenliste);
+			}
+		}
+		{
+			JScrollPane scrollPane = new JScrollPane();
+			scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			scrollPane.setPreferredSize(new Dimension(250, 200));
+			contentPanel.add(scrollPane, BorderLayout.EAST);
+			
+			{
+				//JTextArea Zusammenfassung = new JTextArea();
+				scrollPane.setViewportView(Zusammenfassung);
+				Zusammenfassung.setEditable(false);
+				Zusammenfassung.setFont(new Font("Tahoma", Font.PLAIN, 12));
+				Zusammenfassung.setBorder(UIManager.getBorder("TextField.border"));
+				Zusammenfassung.setWrapStyleWord(true);
+				Zusammenfassung.setLineWrap(true);
+				Zusammenfassung.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+				Zusammenfassung.setText("Hier wird (bei Auswahl einer Klasse) eine Zusammenfassung für die gewählte Klasse angezeigt\r\n");
+			}
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
